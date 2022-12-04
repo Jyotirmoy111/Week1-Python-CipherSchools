a47f670f91c1b294b35a3051ee6f1bcdf7a60e02
Jyotirmoy111 <115767089+Jyotirmoy111@users.noreply.github.com>
2022-12-04 18:23:41 +0530

    Update and rename Ch1Ex1.py to Exercise1Chapter1:Python_tutorial-10(ExerciseBasedOnEscapeSequence)-.py

--- Ch1Ex1.py
+++
@@ -1,4 +0,0 @@
-print("this is \\\\ double blackkslash")
-print("these are /\\/\\/\\/\\/\\ mountains")
-print("he is\tawsome")
-print(" \\\" \\n \\t \\\'")
---
+++ Exercise1Chapter1:Python_tutorial-10(ExerciseBasedOnEscapeSequence)-.py
@@ -0,0 +1,13 @@
+#****** print these following lines ******
+
+#These is \\ double backslash
+print("this is \\\\ double blackkslash")
+
+#These are /\/\/\/\/\ mountains
+print("these are /\\/\\/\\/\\/\\ mountains")
+
+#he is   awsome (use escape sequence insted of manual spaces )
+print("he is\tawsome")
+
+#\" \n \t \'  (print this as an output)
+print(" \\\" \\n \\t \\\'")
