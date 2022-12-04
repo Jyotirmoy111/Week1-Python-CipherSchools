82f1e485cce92752bd583f1aad1d3a2dcc25bd06
Jyotirmoy111 <115767089+Jyotirmoy111@users.noreply.github.com>
2022-12-04 23:01:07 +0530

    Delete Ch2Ex3.py

--- Ch2Ex3.py
+++
@@ -1,5 +0,0 @@
-name, word=input("Enter your name and char by comma separate:").split(",")
-#x=word.lower()
-print("Lengh of the name is:",(len(name)))
-#print(f"No. of times {name} is their:",x.count(x))
-print(f"No. of times {word} is their:{name.lower().count(word.lower())}")
