a27fac2c0dfa9a2774b051ae929b8e4dc80c1a4a
Jyotirmoy111 <115767089+Jyotirmoy111@users.noreply.github.com>
2022-12-04 23:01:45 +0530

    Delete Ch3Ex2.py

--- Ch3Ex2.py
+++
@@ -1,6 +0,0 @@
-name=input("Enter your name:")
-age=int(input("Enter your age:"))
-if age>=10 and (name[0]=='a' or name[0]=='A'):
-    print("You can watch the COCO movie :)")
-else:
-    print("You cann't watch COCO movie :(")
